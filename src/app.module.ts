d898efecfe63f9b4db613dda5d0731470f99d3ce
Joy Baruah <joy.baruah@wireonelabs.com>
2023-07-16 21:46:36 +0530

    feat(structure): typeorm,commitzen
    typeorm setup with db, entry file structured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
+import 'dotenv/config'
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+// DATABASE
+const DATABASE = TypeOrmModule.forRoot({
+  type: "postgres",
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [],
+  synchronize: false
+})
+
+// APP
+const APPS = [
+  UsersModule,
+]
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [...APPS, DATABASE],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
